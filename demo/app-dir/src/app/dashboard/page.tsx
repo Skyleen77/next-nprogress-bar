9c0ef3026347b749353dbf82ed43610ab9fb096b
savicaleksa <aleksa.savic91@gmail.com>
2023-08-06 15:01:39 +0200

    feat: add router.back() examples on dashboard page in app router

--- demo/app-dir/src/app/dashboard/page.tsx
+++ demo/app-dir/src/app/dashboard/page.tsx
@@ -1,9 +1,16 @@
-import Link from 'next/link';
+"use client";
+import Link from "next/link";
+import { useRouter } from "../../../../../dist";
 
 const Dashboard = () => {
+  const router = useRouter();
   return (
     <main>
       <Link href="/">Home</Link>
+      <button onClick={() => router.back()}>Back</button>
+      <button onClick={() => router.back({ showProgressBar: true })}>
+        Back with progress bar
+      </button>
     </main>
   );
 };
