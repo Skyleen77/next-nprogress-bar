f797c5d540dd313d77e48b17737485e877c236cf
tobey <tobey@tobey.codes>
2024-02-15 14:09:00 -0600

    Update sameURL.ts

--- src/utils/sameURL.ts
+++ src/utils/sameURL.ts
@@ -1,7 +1,7 @@
 export function isSameURL(target: URL, current: URL) {
-  const cleanTarget = target.protocol + '//' + target.host + target.pathname;
+  const cleanTarget = target.protocol + '//' + target.host + target.pathname + target.search;
   const cleanCurrent =
-    current.protocol + '//' + current.host + current.pathname;
+    current.protocol + '//' + current.host + current.pathname + current.search;
 
   return cleanTarget === cleanCurrent;
 }
