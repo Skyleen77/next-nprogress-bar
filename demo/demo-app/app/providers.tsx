c44a43c4fca9e08d72be22fb763f247e0ae2c5a8
Elliot Sutton <elsk8@outlook.fr>
2024-03-01 23:10:47 +0100

    feat: update to 2.3.1

--- demo/demo-app/app/providers.tsx
+++ demo/demo-app/app/providers.tsx
@@ -1,8 +1,7 @@
 'use client';
 
 import { useProgress } from '@/context/progress-context';
-// import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
-import { AppProgressBar as ProgressBar } from '../../../dist';
+import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   const { color, height, showSpinner } = useProgress();
