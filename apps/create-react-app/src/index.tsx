8b1cdd19b45187265d8f43f53456a038cb78e476
Elliot Sutton <elsk8@outlook.fr>
2025-02-19 22:51:01 +0100

    feat(core): improve exports

--- apps/create-react-app/src/index.tsx
+++ apps/create-react-app/src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { ProgressProvider } from '@bprogress/react';
 
 const root = ReactDOM.createRoot(
@@ -15,8 +14,3 @@ root.render(
     </ProgressProvider>
   </React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
